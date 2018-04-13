dec143210a29097f0c4b789dd8915c072ff2d5be
Naugtur <naugtur@gmail.com>
2018-04-13 22:48:41 +0200

    todo images

--- z/z.js
+++ z/z.js
@@ -2,8 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const tip = document.querySelector('#tip')
   tip.addEventListener('mouseenter', () => {
     document.body.style.backgroundImage = 'url(./todo.png)'
+    document.body.classList.add('tipped')
   })
   tip.addEventListener('mouseleave', () => {
     document.body.style.backgroundImage = 'none'
+    document.body.classList.remove('tipped')
   })
 })
