90da90a31c858561494b82edae8ff606e196ece1
Zbyszek Tenerowicz <naugtur@gmail.com>
2018-04-14 10:21:10 +0200

    Glitch (#1)
    * 🐿🌮 Updated with Glitch
    
    * 🏗🐸 Updated with Glitch
    
    * cleanup

--- z/z.js
+++ z/z.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+  if (window.location.protocol !== 'file:' && window.location.pathname !== '/') {
+    const back = document.createElement('a')
+    back.innerText = '< menu'
+    back.classList.add('goback')
+    back.addEventListener('click', () => {
+      sessionStorage.removeItem('prevLocation')
+      window.location.href = '/'
+    })
+    document.body.appendChild(back)
+  }
+
   const tip = document.querySelector('#tip')
+  if (!tip) { return }
+
   tip.addEventListener('mouseenter', () => {
     document.body.style.backgroundImage = 'url(./todo.png)'
     document.body.classList.add('tipped')
@@ -9,3 +22,13 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.remove('tipped')
   })
 })
+
+if (window.location.protocol !== 'file:') {
+  if (window.location.pathname === '/') {
+    if (sessionStorage.prevLocation) {
+      window.location.href = sessionStorage.prevLocation
+    }
+  } else {
+    sessionStorage.prevLocation = window.location.pathname
+  }
+}
